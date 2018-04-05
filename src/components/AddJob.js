84408922e81373689184f6726ffac15ad020aab9
tjallen27 <tjallen91@hotmail.co.uk>
2018-04-05 13:13:26 +0100

    Console log new job submittions

--- src/components/AddJob.js
+++ src/components/AddJob.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 
 class AddJob extends Component {
+  constructor(){
+    super();
+    this.state = {
+      newJob: {}
+    }
+  }
+
   static defaultProps = {
     catergories: ['React', 'Node']
   }
 
+  handleSubmit(e){
+    if(this.refs.title.value === ''){
+      alert('Title is required');
+    } else {
+      this.setState({newJob:{
+        title: this.refs.title.value,
+        catergory: this.refs.catergory.value,
+        salary: this.refs.salary.value
+      }}, function(){
+        console.log(this.state);
+      });
+    }
+    e.preventDefault();
+  }
+
   render(){
     let catergoryOptions = this.props.catergories.map(catergory => {
-      return <option key="catergory" value={catergory}>{catergory}</option>
+      return <option key={catergory} value={catergory}>{catergory}</option>
     })
 
     return (
       <div>
         <h3>Add Job</h3>
-        <form>
+        <form onSubmit={this.handleSubmit.bind(this)}>
           <div>
             <label>Catergory</label>
             <select ref="catergory">
@@ -28,6 +50,7 @@ class AddJob extends Component {
             <label>Salary</label>
             <input type="text" ref="salary" />
           </div>
+          <button>Submit</button>
         </form>
       </div>
 
