c72a8e42a6e74e80a9bfdbb8761986a88eafc5d8
tjallen27 <tjallen91@hotmail.co.uk>
2018-04-05 09:39:40 +0100

    Header component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header />
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render(){
+    return (
+      <header className="App-header">
+        <h1 className="App-title">The React Job Board</h1>
+      </header>
+    );
+  }
+};
