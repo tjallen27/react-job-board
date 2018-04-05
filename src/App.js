a6f09dd8b02ae0ff4557013cb3567c55679e3d55
tjallen27 <tjallen91@hotmail.co.uk>
2018-04-05 12:17:54 +0100

    Job component fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
-import RenderFrame from './components/RenderFrame';
+import Jobs from './components/Jobs';
 
 class App extends Component {
+
+  constructor(){
+      super();
+    this.state = {
+      jobs: [
+        {
+          title: 'Junior React Developer',
+          salary: '28000'
+        },
+        {
+          title: 'Junior Node Developer',
+          salary: '34000'
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
-        <div className="jobList">
-          <ul>
-            <li>Job Tite</li>
-            <li>Job Salary</li>
-            <li>Job Description</li>
-            <li>Job created</li>
-          </ul>
-        </div>
-        <div className="renderFrame">
-          <RenderFrame />
-        </div>
+        <Jobs jobs={this.state.jobs}/>
       </div>
     );
   }
---
+++ src/components/Jobs.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Job extends Component {
+  render(){
+    console.log(this.props)
+    return (
+      <h3>Jobs</h3>
+    );
+  }
+}
+
+export default Job;
